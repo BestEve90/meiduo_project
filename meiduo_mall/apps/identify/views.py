74d15a54bc5860fb43c8964b7cf86b5762199933
BestEve <hxhwoaiyanan@163.com>
2019-04-23 22:30:18 +0800

    注册-将验证码保存在redis中

--- meiduo_mall/apps/identify/views.py
+++ meiduo_mall/apps/identify/views.py
@@ -2,15 +2,18 @@ import django_redis
 from django import http
 from django.shortcuts import render
 
-# Create your views here.
 from django.views import View
 
-from meiduo_mall import libs
+from identify import constants
+
 from meiduo_mall.libs.captcha.captcha import captcha
 
 
 class ImageIdentify(View):
     def get(self,request,uuid):
+        # 生成图形验证码
         text,code,image = captcha.generate_captcha()
-        conn = django_redis.get_redis_connection('')
+        # 将验证码保存在redis中
+        conn = django_redis.get_redis_connection('image_code')
+        conn.setex(uuid,constants.IMAGE_CODE_EXPIRES,code)
         return http.HttpResponse(image,content_type='image/png')
