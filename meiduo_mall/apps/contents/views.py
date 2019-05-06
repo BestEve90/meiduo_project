e13d8a3b593fdb4e18524f532db530017c18b63f
BestEve <hxhwoaiyanan@163.com>
2019-05-06 21:02:47 +0800

    首页商品分类展示

--- meiduo_mall/apps/contents/views.py
+++ meiduo_mall/apps/contents/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.views import View
+from meiduo_mall.utils.category import get_categories
 
 
 class IndexView(View):
     def get(self, request):
-        return render(request, 'index.html')
-
+        categories = get_categories()
+        context = {
+            'categories': categories
+        }
+        return render(request, 'index.html', context)
---
+++ meiduo_mall/utils/category.py
@@ -0,0 +1,40 @@
+from goods.models import GoodsChannel
+
+
+def get_categories():
+    '''
+    categories={
+        'channels.id':{
+            'cat1':[{
+                'name':channel.category.name,
+                'url':channel.url
+                },
+            ]
+            'cat2':[{
+                'name':***,
+                'subs':[]
+                },
+            ]
+        },
+    }
+    '''
+    categories = {}
+    channels = GoodsChannel.objects.all()
+    for channel in channels:
+        if channel.group_id not in categories.keys():
+            categories[channel.group_id] = {
+                'cat1': [],
+                'cat2': []
+            }
+        channel_group = categories[channel.group_id]
+        channel_group['cat1'].append({
+            'name': channel.category.name,
+            'url': channel.url
+        })
+        cat2s = channel.category.subs.all()
+        for cat2 in cat2s:
+            channel_group['cat2'].append({
+                'name': cat2.name,
+                'subs': cat2.subs.all()
+            })
+    return categories
