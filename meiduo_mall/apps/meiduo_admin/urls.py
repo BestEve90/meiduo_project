56470ed5803a62d36e4384111b55da10ec2c8759
BestEve <hxhwoaiyanan@163.com>
2019-05-25 20:25:10 +0800

    日增用户统计

--- meiduo_mall/apps/meiduo_admin/urls.py
+++ meiduo_mall/apps/meiduo_admin/urls.py
@@ -5,4 +5,5 @@ from .views import statistics_view
 urlpatterns = [
     url(r'^authorizations/$', obtain_jwt_token),
     url(r'^statistical/total_count/$', statistics_view.UerTotalCountView.as_view()),
+    url(r'^statistical/day_increment/$', statistics_view.DailyUerIncrementView.as_view()),
 ]
--- meiduo_mall/apps/meiduo_admin/views/statistics_view.py
+++ meiduo_mall/apps/meiduo_admin/views/statistics_view.py
@@ -3,10 +3,22 @@ from users.models import User
 from datetime import date
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.permissions import IsAdminUser
 
 
 class UerTotalCountView(APIView):
+    permission_classes = [IsAdminUser]
+
     def get(self, request):
         now_date = date.today()
         count = User.objects.all().count()
+        return Response({'count': count, 'date': now_date})
+
+
+class DailyUerIncrementView(APIView):
+    permission_classes = [IsAdminUser]
+
+    def get(self, request):
+        now_date = date.today()
+        count = User.objects.filter(date_joined__gte=date.today()).count()
         return Response({'count': count, 'date': now_date})
