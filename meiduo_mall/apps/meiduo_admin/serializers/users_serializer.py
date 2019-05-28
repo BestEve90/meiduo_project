251cdc94bb719c22e7fa3bca09077ee5ff5439f3
BestEve <hxhwoaiyanan@163.com>
2019-05-28 21:45:07 +0800

    用户序列化器-重写create()方法,利用create_user()实现密码加密

--- meiduo_mall/apps/meiduo_admin/serializers/users_serializer.py
+++ meiduo_mall/apps/meiduo_admin/serializers/users_serializer.py
@@ -7,5 +7,17 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['id', 'username', 'mobile', 'email', 'password']
         extra_kwargs = {
-            'password': {'write_only': True}
+            'password': {
+                'write_only': True,
+                'max_length': 20,
+                'min_length': 8
+            },
+            'username': {
+                'max_length': 20,
+                'min_length': 5
+            }
         }
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
